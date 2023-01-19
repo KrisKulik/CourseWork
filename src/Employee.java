3905e3977d31ad6ec0b5db623684c0ab433dc82f
Крис <kristinakulik.1993@gmail.com>
2023-01-19 18:14:21 +0400

    1

---
+++ src/Employee.java
@@ -0,0 +1,60 @@
+        /* 1. Создать класс Employee, который содержит информацию о Ф.И.О., отделе и зарплате сотрудника.
+        Отделы для простоты должны быть названы от 1 до 5.
+           2.Добавить статическую переменную-счетчик, которая будет отвечать за id.*/
+        public class Employee {
+            String surname;
+            String name;
+            String patronymic;
+            int [] department = {1, 2, 3, 4, 5};
+            private int salary;
+            public static int counter = 0;
+            private final int id;
+            Employee [] employees = new Employee[10];
+
+        /* 3. Добавить в класс Employee поле id, которое проставляется из счетчика, а затем счетчик увеличивает свое значение.
+           6. По умолчанию все поля должны передавать через конструктор (кроме id) и заполняться в нем (включая id, который нужно получить из счетчика).*/
+        public Employee (String surname, String name, String patronymic, int department, int salary) {
+            this.name = name;
+            this.surname = surname;
+            this.patronymic = patronymic;
+            this.department = new int[]{department};
+            this.salary = salary;
+            id = counter++;
+        }
+        /* 4.Добавить возможность получать значения полей из Employee (геттеры) для всех полей.*/
+        public String getName() {
+            return name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public String getPatronymic() {
+            return patronymic;
+        }
+
+        public int[] getDepartment() {
+            return department;
+        }
+
+        public int getSalary() {
+            return salary;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public int getCounter() {
+            return counter;
+        }
+        /* 5. Добавить возможность устанавливать значения полей отдела и зарплаты (сеттеры).*/
+        public void setDepartment(int[] department) {
+            this.department = department;
+        }
+        public void setSalary(int salary) {
+                this.salary = salary;
+            }
+        }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Arrays;
+/* Создать внутри класса с методом main поле типа Employee[10],
+    которое будет выполнять роль «хранилища» для записей о сотрудниках.*/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee[] employees = new Employee[10];
+
+        employees[0] = new Employee("Лавров", "Сергей", "Викторович", 1, 20_000);
+        employees[1] = new Employee("Песков", "Дмитрий", "Сергеевич", 1, 20_000);
+        employees[2] = new Employee("Небензя", "Василий", "Алексеевич", 2, 25_000);
+        employees[3] = new Employee("Медведев", "Дмитрий", "Анатольевич", 2, 25_000);
+        employees[4] = new Employee("Рогозин", "Дмитрий", "Олегович", 3, 30_000);
+        employees[5] = new Employee("Пригожин", "Евгений", "Викторович", 3, 30_000);
+        employees[6] = new Employee("Шойгу", "Сергей", "Кужугетович", 4, 35_000);
+        employees[7] = new Employee("Володин", "Вячеслав", "Викторович", 4, 35_000);
+        employees[8] = new Employee("Мишустин", "Михаил", "Владимирович", 5, 40_000);
+        employees[9] = new Employee("Собянин", "Сергей", "Семенович", 5, 40_000);
+
+        System.out.println("Ф.И.О.: " + employees[0].getSurname() + " " + employees[0].getName() + " " + employees[0].getPatronymic() + " " + "Отдел: = " + Arrays.toString(employees[0].getDepartment()) + " " + "Оклад: " + employees[0].getSalary() + " " + "id = " + employees[0].getId());
+        System.out.println("Ф.И.О.: " + employees[1].getSurname() + " " + employees[1].getName() + " " + employees[1].getPatronymic() + " " + "Отдел: = " + Arrays.toString(employees[1].getDepartment()) + " " + "Оклад: " + employees[1].getSalary() + " " + "id = " + employees[1].getId());
+        System.out.println("Ф.И.О.: " + employees[2].getSurname() + " " + employees[2].getName() + " " + employees[2].getPatronymic() + " " + "Отдел: = " + Arrays.toString(employees[2].getDepartment()) + " " + "Оклад: " + employees[2].getSalary() + " " + "id = " + employees[2].getId());
+        System.out.println("Ф.И.О.: " + employees[3].getSurname() + " " + employees[3].getName() + " " + employees[3].getPatronymic() + " " + "Отдел: = " + Arrays.toString(employees[3].getDepartment()) + " " + "Оклад: " + employees[3].getSalary() + " " + "id = " + employees[3].getId());
+        System.out.println("Ф.И.О.: " + employees[4].getSurname() + " " + employees[4].getName() + " " + employees[4].getPatronymic() + " " + "Отдел: = " + Arrays.toString(employees[4].getDepartment()) + " " + "Оклад: " + employees[4].getSalary() + " " + "id = " + employees[4].getId());
+        System.out.println("Ф.И.О.: " + employees[5].getSurname() + " " + employees[5].getName() + " " + employees[5].getPatronymic() + " " + "Отдел: = " + Arrays.toString(employees[5].getDepartment()) + " " + "Оклад: " + employees[5].getSalary() + " " + "id = " + employees[5].getId());
+        System.out.println("Ф.И.О.: " + employees[6].getSurname() + " " + employees[6].getName() + " " + employees[6].getPatronymic() + " " + "Отдел: = " + Arrays.toString(employees[6].getDepartment()) + " " + "Оклад: " + employees[6].getSalary() + " " + "id = " + employees[6].getId());
+        System.out.println("Ф.И.О.: " + employees[7].getSurname() + " " + employees[7].getName() + " " + employees[7].getPatronymic() + " " + "Отдел: = " + Arrays.toString(employees[7].getDepartment()) + " " + "Оклад: " + employees[7].getSalary() + " " + "id = " + employees[7].getId());
+        System.out.println("Ф.И.О.: " + employees[8].getSurname() + " " + employees[8].getName() + " " + employees[8].getPatronymic() + " " + "Отдел: = " + Arrays.toString(employees[8].getDepartment()) + " " + "Оклад: " + employees[8].getSalary() + " " + "id = " + employees[8].getId());
+        System.out.println("Ф.И.О.: " + employees[9].getSurname() + " " + employees[9].getName() + " " + employees[9].getPatronymic() + " " + "Отдел: = " + Arrays.toString(employees[9].getDepartment()) + " " + "Оклад: " + employees[9].getSalary() + " " + "id = " + employees[9].getId());
     }
+    public static int calculateSpendingPerMonth(Employee[] employees) {
+        int sum = 0;
+        for (Employee employee: employees) {
+            sum += employee.getSalary();
+        }
+        System.out.println(sum);
+        return sum;
+
+    }
+
+
 }
